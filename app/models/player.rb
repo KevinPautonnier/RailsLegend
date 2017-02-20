6c6f8fd8128a84dfd05d32fcacec989001843726
Kévin PAUTONNIER <kevin.pautonnier@ynov.com>
2017-02-20 11:09:26 +0100

    update player

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,17 +6,22 @@ class Player
 
   def initialize(name, region)
     @key = "RGAPI-d18e1abd-4e19-4453-8249-e10675de1031"
-    @name = name
-    @region = region
+    @name = name.downcase!
+    @region = region.downcase!
 
     response = RestClient.get "https://#{region}.api.pvp.net/api/lol/#{region}/v1.4/summoner/by-name/#{name}?api_key=#{key}"
 
-    response2 = JSON.parse response
+    player_data = JSON.parse response
 
-    player_id = " "
+    print player_data
+
+    player_id = player_data["#{name}"]["id"]
+
+    print player_id
+
     responsefinal = RestClient.get "https://#{region}.api.pvp.net/api/lol/#{region}/v2.2/matchlist/by-summoner/#{player_id}?api_key=#{key}"
 
-    print response2["matratio"]["id"]
+    print responsefinal
 
 
 
