7773eb549e5a81ea957bcde015a9b263b9bf3c6c
mathiasLoiret <mathias44.loiret@hotmail.fr>
2017-02-20 10:44:25 +0100

    first call to api

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,16 +1,26 @@
+require 'rest-client'
 class Player
 
-  attr_accessor :name, :id,
+  attr_accessor :name, :region, :key
 
-  def initialize(name)
+  def initialize(name, region)
+    @key = "RGAPI-d18e1abd-4e19-4453-8249-e10675de1031"
     @name = name
-    @id
-    @profil_icon_id
-    @level
-    @best_champions
+    @region = region
+
+    response = RestClient.get "https://#{region}.api.pvp.net/api/lol/#{region}/v1.4/summoner/by-name/#{name}?api_key=#{key}"
+    print response.class
+    
 
 
+
+    #id = response.print
+    #@profil_icon_id
+    #@level
+    #@best_champions
 
   end
 
 end
+
+Player.new("Matratio", "euw")
